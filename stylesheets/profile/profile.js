e9fb5d8f013ccc983c200cb396e0578b79e53123
mariyasada <mariyasada2406@gmail.com>
2022-02-13 13:59:51 +0530

    doc: changing the ja file code

--- stylesheets/profile/profile.js
+++ stylesheets/profile/profile.js
@@ -8,17 +8,22 @@ let settingdetailDiv = detailcontainer[2];
 
 ProfileBtn.addEventListener( "click",() => {   
    profiledetailDiv.style.display==="none"? profiledetailDiv.style.display = "block": profiledetailDiv.style.display = "none";
-   
+   addressdetailsdiv.style.display="none";
+   settingdetailDiv.style.display="none";
+
 })
 
 
 addressBtn.addEventListener( "click",() => {
     console.log("clicked");
     addressdetailsdiv.style.display==="none"? addressdetailsdiv.style.display = "block": addressdetailsdiv.style.display = "none";
-
+    profiledetailDiv.style.display="none";
+    settingdetailDiv.style.display="none";
 })
 settingBtn.addEventListener( "click",() => {
     // console.log("clicked");
    let settingdetailDiv = detailcontainer[2];
    settingdetailDiv.style.display === "none"? settingdetailDiv.style.display = "block": settingdetailDiv.style.display = "none";
+   addressdetailsdiv.style.display="none";
+   profiledetailDiv.style.display="none";
 })
